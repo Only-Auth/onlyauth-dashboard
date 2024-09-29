e93c3259a78e9c89feb57d1a95cff91ef58c7df9
Paritosh Jha <paritoshjha0504@gmail.com>
2024-09-29 11:34:11 +0530

    Added react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { RouterProvider } from 'react-router-dom'
 
 import router from './router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </>
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   )
 }
 
