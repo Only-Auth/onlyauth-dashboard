00ee520a3d08194209c5c696daf70cb9cbd8ad6d
Paritosh Jha <paritoshjha0504@gmail.com>
2024-10-24 18:05:57 +0530

    Added Loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,5 +1,9 @@
 function Loader() {
-  return <div>Loading...</div>
+  return (
+    <div className="w-full h-[400px] flex items-center justify-center">
+      <div className="w-8 h-8 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin"></div>
+    </div>
+  )
 }
 
 export default Loader
